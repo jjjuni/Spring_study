ef4034ad0b6e3ea62cacde022d68cc9753f24569
이준희 <0210leejun@gmail.com>
2025-02-26 16:14:55 +0900

    :sparkles: validation 추가

---
+++ src/main/java/umc/spring/web/dto/MemberDTO/MemberRequestDTO.java
@@ -0,0 +1,22 @@
+package umc.spring.web.dto.MemberDTO;
+
+import lombok.Getter;
+import umc.spring.validation.annotation.ExistCategories;
+
+import java.util.List;
+
+public class MemberRequestDTO {
+
+    @Getter
+    public static class JoinDTO{
+        String name;
+        Integer gender;
+        Integer birthYear;
+        Integer birthMonth;
+        Integer birthDay;
+        String address;
+        String specAddress;
+        @ExistCategories
+        List<Long> preferCategory;
+    }
+}
